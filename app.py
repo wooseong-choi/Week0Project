45ac8abc1ccc4b0d657b1cc213d727dca0b9a95c
jjr7181 <jjr7181@gmail.com>
2024-03-19 13:38:16 +0900

    connect db

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+from pymongo import MongoClient
+
 from flask import Flask, render_template, jsonify, request
 from flask.json.provider import JSONProvider
 
 app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)
+db = client.dbjungle
+
 
 @app.route('/')
 def home():
