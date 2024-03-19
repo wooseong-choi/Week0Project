604215bfb7034f5eae2141b69e2138f9e48e36ff
wooseong-choi <ghkdgo8686@gmail.com>
2024-03-19 19:52:55 +0900

    테스트 파일 커밋

---
+++ dbtest.py
@@ -0,0 +1,26 @@
+from bson.objectid import ObjectId
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.week0Project
+
+def home():
+
+   #db.users.insert_one({'user_id':'ghkdgo868','password':'dddd','name':'최우성'})
+
+   #db.books.insert_one({'book_name':'신비한 동물사전','book_comment':'이 책은 대단합니다.','book_image':'test.jpg','user_id':'ghkdgo868' })
+   #db.rental.insert_one({'rental_place':'교육관 1층','rental_period':'대여 기간','rental_time':'약속 시간','user_row_id':'test', 'book_row_id':'65f92f7ba31552634ff5638c'})    
+   db.rental.update_one( {'_id':ObjectId('65f969149dff939a81d4cef6')}, {'$set': {'user_row_id':'65f9306e2b8cd2a6b8d19851', 'user_id':'ghkdgo868','take_user_id':'test','book_name':'신비한 동물사전'} } )
+
+
+   print('완료: ')
+
+
+if __name__ == '__main__':  
+    # 기존의 movies 콜렉션을 삭제하기
+    #db.users.drop()
+    #db.books.drop()
+    #db.rental.drop()
+
+    # 영화 사이트를 scraping 해서 db 에 채우기
+    home()    
+
