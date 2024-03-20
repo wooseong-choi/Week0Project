ad4c2fda65eb3476f45bd171dac6a954169f69b2
D_wW <about1201@gmail.com>
2024-03-20 10:38:05 +0900

    commit-login

---
+++ static/script.js
@@ -0,0 +1,61 @@
+function register() {
+    let username = $("#input-username").val()
+    let password = $("#input-password").val()
+    let nickname = $("#input-nickname").val()
+    let password2 = $("#input-password2").val()
+    console.log(username, nickname, password, password2)
+	
+  	
+    $.ajax({
+        type: "POST",
+        url: "/sign_up/save",
+        data: {
+            username_give: username,
+            password_give: password,
+            nickname_give: nickname
+        },
+        success: function (response) {
+            alert("회원가입을 축하드립니다!")
+            window.location.replace("/login")
+        }
+    });
+
+}
+
+
+function sign_in() {
+    let username = $("#input-username").val()
+    let password = $("#input-password").val()
+
+    if (username == "") {
+        $("#help-id-login").text("아이디를 입력해주세요.")
+        $("#input-username").focus()
+        return;
+    } else {
+        $("#help-id-login").text("")
+    }
+
+    if (password == "") {
+        $("#help-password-login").text("비밀번호를 입력해주세요.")
+        $("#input-password").focus()
+        return;
+    } else {
+        $("#help-password-login").text("")
+    }
+    $.ajax({
+        type: "POST",
+        url: "/sign_in",
+        data: {
+            username_give: username,
+            password_give: password
+        },
+        success: function (response) {
+            if (response['result'] == 'success') {
+                $.cookie('mytoken', response['token'], {path: '/'});
+                window.location.replace("/")
+            } else {
+                alert(response['msg'])
+            }
+        }
+    });
+}
